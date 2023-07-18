e4661f5a948872fc66896441e58cb1c5fc0828f0
dev-marko <markospasenovski00@gmail.com>
2023-07-18 23:18:25 +0200

    Add UserService

---
+++ FloraEdu.Application/Authentication/Implementations/UserService.cs
@@ -0,0 +1,170 @@
+﻿using System.Security.Claims;
+using FloraEdu.Application.Authentication.Dtos;
+using FloraEdu.Application.Authentication.Dtos.Extensions;
+using FloraEdu.Application.Authentication.Interfaces;
+using FloraEdu.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FloraEdu.Application.Authentication.Implementations;
+
+public class UserService : IUserService
+{
+    private readonly UserManager<User> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly SignInManager<User> _signInManager;
+
+    public UserService(UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
+        SignInManager<User> signInManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _signInManager = signInManager;
+    }
+
+    public async Task<IEnumerable<UserDto>> GetAll()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        var userDtos = users.Select(user => user.MapToDto());
+        return userDtos;
+    }
+
+    public async Task<User> FindByNameAsync(string userName)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user == null)
+            throw new Exception("User not found");
+        return user;
+    }
+
+    public async Task<User> FindByIdAsync(Guid id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+        if (user == null)
+            throw new Exception("User not found");
+        return user;
+    }
+
+
+    public async Task<User> FindByEmailAsync(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        if (user is null) throw new Exception("User not found");
+        return user;
+    }
+
+    public async Task Update(string id, UserDto user)
+    {
+        var userToUpdate = await _userManager.FindByIdAsync(id.ToString());
+        if (userToUpdate == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        userToUpdate.FirstName = user.FirstName;
+        userToUpdate.LastName = user.LastName;
+        userToUpdate.Email = user.Email;
+        userToUpdate.IsDeleted = user.IsDeleted;
+        await _userManager.UpdateAsync(userToUpdate);
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var userToDelete = await _userManager.FindByIdAsync(id.ToString());
+        if (userToDelete == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        userToDelete.IsDeleted = true;
+        await _userManager.UpdateAsync(userToDelete);
+    }
+
+
+    public async Task<bool> CheckPasswordAsync(string userName, string password)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user == null)
+            throw new Exception("User not found");
+        var passwordMatches = await _userManager.CheckPasswordAsync(user, password);
+        if (!passwordMatches)
+            throw new InvalidOperationException("Password doesn't match");
+        return passwordMatches;
+    }
+
+    public async Task<IList<string>> GetRolesAsync(LoginDto user)
+    {
+        var existingUser = await _userManager.FindByNameAsync(user.UserName);
+        if (existingUser == null)
+            throw new Exception("User not found");
+        return await _userManager.GetRolesAsync(existingUser);
+    }
+
+    public async Task<IdentityResult> CreateUserAsync(User user, string password)
+    {
+        var result = await _userManager.CreateAsync(user, password);
+        if (!result.Succeeded)
+            throw new Exception("Operation Failed");
+        return result;
+    }
+
+    public async Task<bool> RoleExistsAsync(string roleName)
+    {
+        return await _roleManager.RoleExistsAsync(roleName);
+    }
+
+    public async Task<IdentityResult?> AddToRoleAsync(User user, string role)
+    {
+        if (await _roleManager.RoleExistsAsync(role))
+        {
+            return await _userManager.AddToRoleAsync(user, role);
+        }
+
+        return null;
+    }
+
+    public async Task<IdentityResult> CreateRoleAsync(IdentityRole role)
+    {
+        return await _roleManager.CreateAsync(role);
+    }
+
+    public async Task<bool> UserExists(string userName)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user == null)
+            return false;
+        return true;
+    }
+
+    public async Task Login(string userName, string password, string jwt)
+    {
+        var user = await _userManager.FindByNameAsync(userName);
+        if (user == null)
+            throw new Exception("User not found");
+        await _signInManager.PasswordSignInAsync(userName, password, true, true);
+        await _userManager.AddClaimAsync(user, new Claim("Name", userName));
+    }
+
+    public async Task Logout(Guid userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId.ToString());
+        if (user == null)
+            throw new Exception("User not found");
+        await _signInManager.SignOutAsync();
+    }
+
+    public async Task<ExternalLoginInfo?> GetExternalLoginInfoAsync()
+    {
+        return await _signInManager.GetExternalLoginInfoAsync();
+    }
+
+    public async Task<IdentityResult> AddLoginAsync(User user, UserLoginInfo login)
+    {
+        return await _userManager.AddLoginAsync(user, login);
+    }
+
+    public async Task SignInAsync(User user, bool isPersistent)
+    {
+        await _signInManager.SignInAsync(user, isPersistent);
+    }
+}
---
+++ FloraEdu.Application/Authentication/Interfaces/IUserService.cs
@@ -0,0 +1,27 @@
+﻿using FloraEdu.Application.Authentication.Dtos;
+using FloraEdu.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace FloraEdu.Application.Authentication.Interfaces;
+
+public interface IUserService
+{
+    Task<IEnumerable<UserDto>> GetAll();
+    Task<User> FindByNameAsync(string userName);
+    Task<User> FindByIdAsync(Guid id);
+    Task<User> FindByEmailAsync(string email);
+    Task Update(string id, UserDto entity);
+    Task Delete(Guid id);
+    Task<bool> CheckPasswordAsync(string userName, string password);
+    Task<IList<string>> GetRolesAsync(LoginDto user);
+    Task<IdentityResult> CreateUserAsync(User user, string password);
+    Task<bool> RoleExistsAsync(string roleName);
+    Task<IdentityResult?> AddToRoleAsync(User user, string role);
+    Task<IdentityResult> CreateRoleAsync(IdentityRole role);
+    Task<bool> UserExists(string userName);
+    Task Login(string userName, string password, string jwt);
+    Task Logout(Guid userId);
+    Task<ExternalLoginInfo?> GetExternalLoginInfoAsync();
+    Task<IdentityResult> AddLoginAsync(User user, UserLoginInfo login);
+    Task SignInAsync(User user, bool isPersistent);
+}
