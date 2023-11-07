6fc0430901197e172bb0ee28897aae9954943955
dev-marko <markospasenovski00@gmail.com>
2023-11-07 23:54:55 +0100

    Update ApplicationDbContext

--- FloraEdu.Persistence/Migrations/20231107223728_AddPlantAndCommentEntity.Designer.cs
+++ FloraEdu.Persistence/Migrations/20231107223728_AddPlantAndCommentEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FloraEdu.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FloraEdu.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231107223728_AddPlantAndCommentEntity")]
+    partial class AddPlantAndCommentEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
