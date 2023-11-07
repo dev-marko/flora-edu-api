8186be5ef4895403149477cd49c52b08c3a41e9f
dev-marko <markospasenovski00@gmail.com>
2023-11-07 23:50:37 +0100

    Add new PlantComment entity

---
+++ FloraEdu.Domain/Entities/PlantComment.cs
@@ -0,0 +1,10 @@
+﻿namespace FloraEdu.Domain.Entities;
+
+public class PlantComment : BaseEntity
+{
+    public Guid PlantId { get; set; }
+    public required Plant Plant { get; set; }
+    public required string UserId { get; set; }
+    public required User User { get; set; }
+    public required string Content { get; set; }
+}
