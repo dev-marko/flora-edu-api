f33cf6bff7a102d9414471179cb43a4ff66e427b
dev-marko <markospasenovski00@gmail.com>
2023-11-07 23:54:38 +0100

    Add User, Plant and PlantComment entity configs

---
+++ FloraEdu.Persistence/Configurations/PlantConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using FloraEdu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FloraEdu.Persistence.Configurations;
+
+public class PlantConfiguration : IEntityTypeConfiguration<Plant>
+{
+    public void Configure(EntityTypeBuilder<Plant> builder)
+    {
+        builder.ToTable("Plants");
+        builder.Property(plant => plant.Id).ValueGeneratedOnAdd();
+    }
+}
