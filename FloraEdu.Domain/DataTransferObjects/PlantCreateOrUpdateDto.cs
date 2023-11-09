24025908e63f5300b3d77f43525a3559d26994f2
dev-marko <markospasenovski00@gmail.com>
2023-11-09 21:40:07 +0100

    Register validators for PlantDto

---
+++ FloraEdu.Domain/DataTransferObjects/PlantCreateOrUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using FloraEdu.Domain.Enumerations;
+
+namespace FloraEdu.Domain.DataTransferObjects;
+
+public class PlantCreateOrUpdateDto
+{
+    public required string Name { get; set; }
+    public required PlantType Type { get; init; } = 0;
+    public string? Description { get; set; }
+    public string? Predispositions { get; set; }
+    public string? Planting { get; set; }
+    public string? Maintenance { get; set; }
+}
