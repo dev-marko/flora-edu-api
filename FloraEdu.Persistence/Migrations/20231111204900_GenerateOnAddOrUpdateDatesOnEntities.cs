259d164731e9c38bf65e5237c4f056a157c66c3e
dev-marko <markospasenovski00@gmail.com>
2023-11-11 22:33:31 +0100

    Update entity configurations

---
+++ FloraEdu.Persistence/Migrations/20231111204900_GenerateOnAddOrUpdateDatesOnEntities.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FloraEdu.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenerateOnAddOrUpdateDatesOnEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,nuts,shrubs,tree,flower,herbs,other")
+                .OldAnnotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,tree,flower,herbs,other");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,tree,flower,herbs,other")
+                .OldAnnotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,nuts,shrubs,tree,flower,herbs,other");
+        }
+    }
+}
---
+++ FloraEdu.Persistence/Migrations/20231111210336_AutoGeneratePrimaryKeyGuid.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FloraEdu.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoGeneratePrimaryKeyGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,nuts,shrubs,tree,flower,herbs,other")
+                .Annotation("Npgsql:PostgresExtension:uuid-ossp", ",,")
+                .OldAnnotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,nuts,shrubs,tree,flower,herbs,other");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Plants",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "PlantImageUrls",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "PlantComments",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "uuid_generate_v4()",
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,nuts,shrubs,tree,flower,herbs,other")
+                .OldAnnotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,nuts,shrubs,tree,flower,herbs,other")
+                .OldAnnotation("Npgsql:PostgresExtension:uuid-ossp", ",,");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Plants",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuid_generate_v4()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "PlantImageUrls",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuid_generate_v4()");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "PlantComments",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "uuid_generate_v4()");
+        }
+    }
+}
