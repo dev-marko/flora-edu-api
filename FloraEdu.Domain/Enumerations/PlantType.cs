4551369e1fa478c55e042277caae4a56e087ccb9
dev-marko <markospasenovski00@gmail.com>
2023-11-11 22:33:53 +0100

    Add new plant types

--- FloraEdu.Domain/Enumerations/PlantType.cs
+++ FloraEdu.Domain/Enumerations/PlantType.cs
@@ -2,9 +2,13 @@ namespace FloraEdu.Domain.Enumerations;
 
 public enum PlantType
 {
-    Unknown,
-    Flower,
-    Fruit,
-    Vegetable,
-    Tree
+    Unknown = 0,
+    Fruit = 1,
+    Vegetable = 2,
+    Nuts = 3,   // Jatkasti plodovi
+    Shrubs = 4, // Grmushki
+    Tree = 5,
+    Flower = 6,
+    Herbs = 7, // Bilki
+    Other = 8
 }
