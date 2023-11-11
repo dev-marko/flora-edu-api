7808c75a9ccc5ccf6fac39b612eb220876329b6b
dev-marko <markospasenovski00@gmail.com>
2023-11-11 22:33:03 +0100

    Add new PlantImage entity and configuration

---
+++ FloraEdu.Persistence/Configurations/PlantImageConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using FloraEdu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FloraEdu.Persistence.Configurations;
+
+public class PlantImageConfiguration : IEntityTypeConfiguration<PlantImage>
+{
+    public void Configure(EntityTypeBuilder<PlantImage> builder)
+    {
+        builder.ToTable("PlantImageUrls");
+        
+        builder.Property(plantImageObj => plantImageObj.Id).HasDefaultValueSql("uuid_generate_v4()");
+        
+        builder
+            .Property(plantImageObj => plantImageObj.CreatedAt)
+            .HasDefaultValueSql("now() at time zone 'utc'")
+            .ValueGeneratedOnAdd();
+        
+        builder
+            .Property(plantImageObj => plantImageObj.LastModified)
+            .HasDefaultValueSql("now() at time zone 'utc'")
+            .ValueGeneratedOnAddOrUpdate();
+        
+        builder.Property(plantImageObj => plantImageObj.IsDeleted).HasDefaultValue(false);
+    }
+}
---
+++ FloraEdu.Persistence/Migrations/20231111202455_AddPlantImageEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FloraEdu.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlantImageEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,tree,flower,herbs,other");
+
+            migrationBuilder.CreateTable(
+                name: "PlantImageUrls",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlantId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ThumbnailImageUrl = table.Column<string>(type: "text", nullable: true),
+                    HeaderImageUrls = table.Column<List<string>>(type: "text[]", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlantImageUrls", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlantImageUrls_Plants_PlantId",
+                        column: x => x.PlantId,
+                        principalTable: "Plants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlantImageUrls_PlantId",
+                table: "PlantImageUrls",
+                column: "PlantId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlantImageUrls");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:Enum:plant_type", "unknown,fruit,vegetable,tree,flower,herbs,other");
+        }
+    }
+}
