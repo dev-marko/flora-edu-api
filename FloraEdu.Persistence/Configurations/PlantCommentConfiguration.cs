b8fcfc5fec28330f683c87dbf0b777126ea1ba0b
dev-marko <markospasenovski00@gmail.com>
2023-11-12 23:02:26 +0100

    Refactor entity configurations to use BaseEntityConfiguration

--- FloraEdu.Persistence/Configurations/PlantCommentConfiguration.cs
+++ FloraEdu.Persistence/Configurations/PlantCommentConfiguration.cs
@@ -4,24 +4,12 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FloraEdu.Persistence.Configurations;
 
-public class PlantCommentConfiguration : IEntityTypeConfiguration<PlantComment>
+public class PlantCommentConfiguration : BaseEntityConfiguration<PlantComment>
 {
-    public void Configure(EntityTypeBuilder<PlantComment> builder)
+    public override void Configure(EntityTypeBuilder<PlantComment> builder)
     {
+        base.Configure(builder);
+        
         builder.ToTable("PlantComments");
-        
-        builder.Property(plantComment => plantComment.Id).HasDefaultValueSql("uuid_generate_v4()");
-        
-        builder
-            .Property(plantComment => plantComment.CreatedAt)
-            .HasDefaultValueSql("now() at time zone 'utc'")
-            .ValueGeneratedOnAdd();
-
-        builder
-            .Property(plantComment => plantComment.LastModified)
-            .HasDefaultValueSql("now() at time zone 'utc'")
-            .ValueGeneratedOnAddOrUpdate();
-
-        builder.Property(plantComment => plantComment.IsDeleted).HasDefaultValue(false);
     }
 }
