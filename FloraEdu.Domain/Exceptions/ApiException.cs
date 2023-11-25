05ea84ca87b15b826e0d7511af8c9eaa5d7c0a7d
dev-marko <markospasenovski00@gmail.com>
2023-11-26 00:16:52 +0100

    Add new custom exceptions
    OperationFailed, UserNotFound, PasswordMismatch

--- FloraEdu.Domain/Exceptions/ApiException.cs
+++ FloraEdu.Domain/Exceptions/ApiException.cs
@@ -19,6 +19,10 @@ public static class ApiExceptionExtension
         { ErrorCodes.NotFound, HttpStatusCode.NotFound },
         { ErrorCodes.BadRequest, HttpStatusCode.BadRequest },
         { ErrorCodes.InternalServerError, HttpStatusCode.InternalServerError },
+        { ErrorCodes.OperationFailed, HttpStatusCode.InternalServerError },
+
+        { ErrorCodes.UserNotFound, HttpStatusCode.NotFound },
+        { ErrorCodes.PasswordMismatch, HttpStatusCode.BadRequest }
     };
 
     public static HttpStatusCode GetStatusCode(string errorMessage)
