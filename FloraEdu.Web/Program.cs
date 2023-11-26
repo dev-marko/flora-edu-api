c94720ce05e10243abdc7dfb862a98086b44be04
dev-marko <markospasenovski00@gmail.com>
2023-11-27 00:03:51 +0100

    Edit JWT configuration and add policies

--- FloraEdu.Web/Program.cs
+++ FloraEdu.Web/Program.cs
@@ -1,13 +1,17 @@
+using System.Security.Claims;
 using FloraEdu.Application.Authentication.Implementations;
 using FloraEdu.Application.Authentication.Interfaces;
+using FloraEdu.Domain.Authorization;
 using FloraEdu.Domain.DataTransferObjects.Plant;
 using FloraEdu.Domain.Entities;
+using FloraEdu.Domain.Enumerations;
 using FloraEdu.Domain.Validators;
 using FloraEdu.Persistence;
 using FloraEdu.Web.Middlewares;
 using FloraEdu.Web.Options;
 using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,14 +29,35 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 builder.Services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions.PropertyNameCaseInsensitive = true);
 
-builder.Services.AddIdentity<User, IdentityRole>()
+builder.Services
+    .AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer();
 
 builder.Services.ConfigureOptions<JwtOptionsSetup>();
 builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy(AuthorizationPolicies.Authenticated, policy => policy.RequireAuthenticatedUser())
+    .AddPolicy(AuthorizationPolicies.Admin, policy => policy.RequireRole(Roles.Admin))
+    .AddPolicy(AuthorizationPolicies.Specialist, policy => policy.RequireRole(Roles.Specialist))
+    .AddPolicy(AuthorizationPolicies.AdminOrSpecialist, policy => policy.RequireRole(Roles.Admin, Roles.Specialist))
+    .AddPolicy(AuthorizationPolicies.RegularUser, policy => policy.RequireRole(Roles.RegularUser));
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = 401;
+        return Task.CompletedTask;
+    };
+});
 
 // Add services to the container.
 builder.Services.AddScoped<IJwtProvider, JwtProvider>();
