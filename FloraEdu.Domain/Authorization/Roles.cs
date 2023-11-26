5893cff514d920d143ab67a02aeee06b46f3c678
dev-marko <markospasenovski00@gmail.com>
2023-11-27 00:02:57 +0100

    Add user roles and authorization policies

---
+++ FloraEdu.Domain/Authorization/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace FloraEdu.Domain.Authorization;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Specialist = "Specialist";
+    public const string RegularUser = "RegularUser";
+}
