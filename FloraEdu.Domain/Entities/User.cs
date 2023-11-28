65d9590d9767711d3219a33c1aa5c283152e97f2
dev-marko <markospasenovski00@gmail.com>
2023-11-29 00:47:38 +0100

    Add new AvatarImageUrl property to User entity

--- FloraEdu.Domain/Entities/User.cs
+++ FloraEdu.Domain/Entities/User.cs
@@ -6,6 +6,7 @@ public class User : IdentityUser
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? AvatarImageUrl { get; set; }
     public bool IsDeleted { get; set; }
     public List<Plant> LikedPlants { get; set; } = new();
     public List<Plant> BookmarkedPlants { get; set; } = new();
