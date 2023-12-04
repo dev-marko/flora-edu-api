b0a0f06ae742855bbc6003bad3b9aa7b050a3a75
dev-marko <markospasenovski00@gmail.com>
2023-12-05 00:21:00 +0100

    Edit PagedList and PlantService

--- FloraEdu.Domain/DataTransferObjects/PagedList.cs
+++ FloraEdu.Domain/DataTransferObjects/PagedList.cs
@@ -4,13 +4,8 @@ namespace FloraEdu.Domain.DataTransferObjects;
 
 public class PagedList<T>
 {
-    private PagedList(List<T> items, int page, int pageSize, int totalCount)
-    {
-        Items = items;
-        Page = page;
-        PageSize = pageSize;
-        TotalCount = totalCount;
-    }
+    private const int DefaultPage = 1;
+    private const int DefaultPageSize = 10;
 
     public List<T> Items { get; }
     public int Page { get; }
@@ -19,8 +14,19 @@ public class PagedList<T>
     public bool HasNextPage => Page * PageSize < TotalCount;
     public bool HasPreviousPage => Page > 1;
 
+    private PagedList(List<T> items, int page, int pageSize, int totalCount)
+    {
+        Items = items;
+        Page = page;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+
     public static async Task<PagedList<T>> CreateAsync(IQueryable<T> query, int page, int pageSize)
     {
+        page = page == 0 ? DefaultPage : page;
+        pageSize = pageSize == 0 ? DefaultPageSize : pageSize;
+
         var totalCount = await query.CountAsync();
         var items = await query.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
 
