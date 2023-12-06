83293729d3b6a115321b17ea45919379fbf1d865
dev-marko <markospasenovski00@gmail.com>
2023-12-07 00:04:20 +0100

    Update ArticleDto to return Content & Subtitle

--- FloraEdu.Domain/DataTransferObjects/Article/ArticleDto.cs
+++ FloraEdu.Domain/DataTransferObjects/Article/ArticleDto.cs
@@ -3,8 +3,10 @@
 public class ArticleDto : BaseDto
 {
     public string? Title { get; set; }
+    public string? Subtitle { get; set; }
     public string? ShortDescription { get; set; }
     public string? HeaderImageUrl { get; set; }
+    public string? Content { get; set; }
     public AuthorDto? Author { get; set; }
     public bool IsLiked { get; set; }
     public bool IsBookmarked { get; set; }
