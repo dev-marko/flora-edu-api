8774abc0145d67672e34082a52700f1f4bb15584
dev-marko <markospasenovski00@gmail.com>
2023-12-12 01:57:27 +0100

    Update models and create DTOs

---
+++ FloraEdu.Domain/DataTransferObjects/Article/NewArticleCommentDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FloraEdu.Domain.DataTransferObjects.Article;
+
+public class NewArticleCommentDto
+{
+    public Guid ArticleId { get; set; }
+    public required string Content { get; set; }
+}
---
+++ FloraEdu.Domain/DataTransferObjects/CommentUserInfoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FloraEdu.Domain.DataTransferObjects;
+
+public class CommentUserInfoDto : BaseDto
+{
+    public required string UserName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? AvatarImageUrl { get; set; }
+}
